b4c5049eaab926c7d4ff22c99db65a0ffd3c2740
taha <ktaha9025@gmail.com>
2024-07-25 15:22:45 +0500

    big changes

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -32,100 +32,6 @@ closeBtn.addEventListener("click", () => {
   menu.classList.remove("active");
 });
 
-$(".slider").each(function () {
-  var $this = $(this);
-  var $group = $this.find(".slide_group");
-  var $slides = $this.find(".slide");
-  var bulletArray = [];
-  var currentIndex = 0;
-  var timeout;
-
-  function move(newIndex) {
-    var animateLeft, slideLeft;
-
-    advance();
-
-    if ($group.is(":animated") || currentIndex === newIndex) {
-      return;
-    }
-
-    bulletArray[currentIndex].removeClass("active");
-    bulletArray[newIndex].addClass("active");
-
-    if (newIndex > currentIndex) {
-      slideLeft = "100%";
-      animateLeft = "-100%";
-    } else {
-      slideLeft = "-100%";
-      animateLeft = "100%";
-    }
-
-    $slides.eq(newIndex).css({
-      display: "block",
-      left: slideLeft,
-    });
-    $group.animate(
-      {
-        left: animateLeft,
-      },
-      function () {
-        $slides.eq(currentIndex).css({
-          display: "none",
-        });
-        $slides.eq(newIndex).css({
-          left: 0,
-        });
-        $group.css({
-          left: 0,
-        });
-        currentIndex = newIndex;
-      }
-    );
-  }
-
-  function advance() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      if (currentIndex < $slides.length - 1) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    }, 4000);
-  }
-
-  $(".next_btn").on("click", function () {
-    if (currentIndex < $slides.length - 1) {
-      move(currentIndex + 1);
-    } else {
-      move(0);
-    }
-  });
-
-  $(".previous_btn").on("click", function () {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1);
-    } else {
-      move(3);
-    }
-  });
-
-  $.each($slides, function (index) {
-    var $button = $('<a class="slide_btn">&bull;</a>');
-
-    if (index === currentIndex) {
-      $button.addClass("active");
-    }
-    $button
-      .on("click", function () {
-        move(index);
-      })
-      .appendTo(".slide_buttons");
-    bulletArray.push($button);
-  });
-
-  advance();
-});
 
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbzbtkL00kRsQyky5m_PrZ0HEg_Cy-AtPsInGoaYVG-Fc0oBq7kvPyDsG0uBY2Zkk0qgdA/exec";
@@ -151,3 +57,90 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   loader.style.display = "none";
 });
+
+
+// slider
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if (carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if (!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if (window.innerWidth < 800 || !isAutoPlay) return;
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
